36645ae2cfebc645653b09bfbc329c45cc84811b
saffront <jiashern@hotmail.com>
2015-10-29 01:39:31 +0000

    jquery library dropdowns working

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,16 +3,22 @@ $(window).load(function(){
   $('h1').addClass('letter-spacing')
 })
 
-function showDiv() {
-   document.getElementById('mainlibrary').style.display = "block";
-}
+// function showDiv() {
+//    document.getElementById('mainlibrary').style.display = "block";
+// }
 
-function showDiv() {
-   document.getElementById('sciencelibrary').style.display = "block";
-}
+// function showDiv() {
+//    document.getElementById('sciencelibrary').style.display = "block";
+// }
 
-function showDiv() {
-   document.getElementById('archaeologylibrary').style.display = "block";
-}
+// function showDiv() {
+//    document.getElementById('archaeologylibrary').style.display = "block";
+// }
 
 
+jQuery(function(){
+        jQuery('.showSingle').click(function(){
+              jQuery('.targetDiv').hide();
+              jQuery('#div'+$(this).attr('target')).show();
+        });
+});
