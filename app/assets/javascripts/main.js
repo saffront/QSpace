e9043112862263704243f235d8e5b4b66898e536
saffront <jiashern@hotmail.com>
2015-10-29 01:57:53 +0000

    updated library dropdowns

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,19 +3,6 @@ $(window).load(function(){
   $('h1').addClass('letter-spacing')
 })
 
-// function showDiv() {
-//    document.getElementById('mainlibrary').style.display = "block";
-// }
-
-// function showDiv() {
-//    document.getElementById('sciencelibrary').style.display = "block";
-// }
-
-// function showDiv() {
-//    document.getElementById('archaeologylibrary').style.display = "block";
-// }
-
-
 jQuery(function(){
         jQuery('.showSingle').click(function(){
               jQuery('.targetDiv').hide();
