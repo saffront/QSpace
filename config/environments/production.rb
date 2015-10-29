ed01d42b2e1d7b25277e2ce501e55895d63fa944
saffront <jiashern@hotmail.com>
2015-10-29 02:00:55 +0000

    javascript not working on deploy fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.compress = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
