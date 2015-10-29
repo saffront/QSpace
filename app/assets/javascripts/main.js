ebe5b0f92bf2ec26a8ba34dbc1da646d4575358e
saffront <jiashern@hotmail.com>
2015-10-29 01:14:57 +0000

    javascript dropdown test

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,3 +2,17 @@ $(window).load(function(){
   $('body').addClass('loaded');
   $('h1').addClass('letter-spacing')
 })
+
+function showDiv() {
+   document.getElementById('mainlibrary').style.display = "block";
+}
+
+function showDiv() {
+   document.getElementById('sciencelibrary').style.display = "block";
+}
+
+function showDiv() {
+   document.getElementById('archaeologylibrary').style.display = "block";
+}
+
+
