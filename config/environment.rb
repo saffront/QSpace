ecc14856bfee4adf00bf0e52e6a9b5ae9983856c
saffront <jiashern@hotmail.com>
2015-11-28 15:40:27 +0000

    percircle fix attempt

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+YourApp::Application.configure do
+  config.less.paths << File.join(Rails.root, 'app', 'assets', 'less')
+end
