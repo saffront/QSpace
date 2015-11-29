f5eb9341bd393bb5c421249551a17a033263416d
saffront <jiashern@hotmail.com>
2015-11-29 00:58:58 +0000

    added fun preloader for map

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,9 @@
+setTimeout(function(){
+  $('body').addClass('loaded');
+  $('h1').css('color', '#222');
+}, 2000);
+  
+
 $(window).load(function(){
   $('body').addClass('loaded');
   $('h1').addClass('letter-spacing')
